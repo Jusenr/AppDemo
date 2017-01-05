5d8ab34da1b439f0b0c6e1bbb952ade74d3ddba7
Jusenr <zhouxingrong@putao.com>
2017-01-05 11:49:49 +0800

    dfsdfd

--- app/src/main/java/com/myself/appdemo/MainActivity.java
+++ app/src/main/java/com/myself/appdemo/MainActivity.java
@@ -1,8 +1,12 @@
 package com.myself.appdemo;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
 
 import com.myself.mylibrary.controller.BasicFragmentActivity;
+
+import butterknife.OnClick;
 
 public class MainActivity extends BasicFragmentActivity {
 
@@ -16,8 +20,48 @@ public class MainActivity extends BasicFragmentActivity {
 
     }
 
+    private void onLeftClick() {
+    }
+
+    private void onMainClick() {
+    }
+
+    private void onRightClick() {
+    }
+
+    @OnClick({R.id.tv_left, R.id.tv_main, R.id.tv_right})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.tv_left:
+                onLeftClick();
+                break;
+            case R.id.tv_main:
+                onMainClick();
+                break;
+            case R.id.tv_right:
+                onRightClick();
+                break;
+        }
+    }
+
+    /**
+     * 返回键
+     *
+     * @param keyCode
+     * @param event
+     * @return
+     */
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            return exit();
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
     @Override
     protected String[] getRequestUrls() {
         return new String[0];
     }
+
 }
