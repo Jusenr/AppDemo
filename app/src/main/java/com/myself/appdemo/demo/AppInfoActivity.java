846ba8d54e641cb369935b88043f82349f1433ad
Jusenr <zhouxingrong@putao.com>
2017-01-13 18:01:58 +0800

    versionName update

--- app/src/main/java/com/myself/appdemo/demo/AppInfoActivity.java
+++ app/src/main/java/com/myself/appdemo/demo/AppInfoActivity.java
@@ -7,11 +7,15 @@ import com.myself.appdemo.Constants;
 import com.myself.appdemo.R;
 import com.myself.appdemo.base.PTWDActivity;
 import com.myself.appdemo.bean.FirInfoBean;
+import com.myself.appdemo.upgrade.UpgradeHelper;
 import com.myself.mylibrary.util.AppUtils;
 import com.myself.mylibrary.util.DateUtils;
 import com.myself.mylibrary.util.FileUtils;
+import com.myself.mylibrary.util.StringUtils;
 
 import butterknife.BindView;
+
+import static android.R.attr.versionName;
 
 public class AppInfoActivity extends PTWDActivity {
 
@@ -48,6 +52,20 @@ public class AppInfoActivity extends PTWDActivity {
         mBean = (FirInfoBean) args.getSerializable(Constants.BundleKey.BUNDLE_APP_INFO);
 
         initView();
+        upgrade();
+    }
+
+    private void upgrade() {
+        if (mBean != null) {
+            String versionShort = mBean.getVersionShort();
+            if (!StringUtils.isEmpty(versionShort)) {
+                String substring = mBean.getVersionShort().substring(1);
+                Float aFloat0 = Float.valueOf(versionName);
+                Float aFloat1 = Float.valueOf(substring);
+                if (aFloat0 < aFloat1)
+                    UpgradeHelper.showUpdateDialog(this, true, mBean);
+            }
+        }
     }
 
     private void initView() {
