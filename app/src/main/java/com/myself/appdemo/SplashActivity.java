844fc0da4cd4e927428b7ceff8e142ab5f2811ff
Jusenr <zhouxingrong@putao.com>
2017-02-07 11:46:55 +0800

    SplashActivity

---
+++ app/src/main/java/com/myself/appdemo/SplashActivity.java
@@ -0,0 +1,65 @@
+package com.myself.appdemo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.AnimationSet;
+import android.view.animation.RotateAnimation;
+import android.view.animation.ScaleAnimation;
+import android.widget.ImageView;
+
+import com.myself.mylibrary.controller.BasicFragmentActivity;
+import com.myself.mylibrary.util.AnimationUtils;
+
+import butterknife.BindView;
+import butterknife.OnClick;
+
+public class SplashActivity extends BasicFragmentActivity {
+    private final int SPLASH_DISPLAY_LENGHT = 5000; //延迟5秒
+
+    @BindView(R.id.splash)
+    ImageView mSplash;
+
+    @Override
+    protected int getLayoutId() {
+        getWindow().requestFeature(Window.FEATURE_PROGRESS); //去标题栏
+        return R.layout.activity_splash;
+    }
+
+    @Override
+    protected void onViewCreatedFinish(Bundle saveInstanceState) {
+        AlphaAnimation alphaAnimation = AnimationUtils.getShowAlphaAnimation(1000);
+        ScaleAnimation scaleAnimation = AnimationUtils.getAmplificationAnimation(800);
+        RotateAnimation rotateAnimation = AnimationUtils.getRotateAnimationByCenter(600);
+        AnimationSet set = AnimationUtils.getAnimationSet();
+        set.addAnimation(alphaAnimation);
+        set.addAnimation(scaleAnimation);
+        set.addAnimation(rotateAnimation);
+        mSplash.setAnimation(set);
+        set.startNow();
+
+//        mSplash.setAnimation(rotateAnimation);
+
+        new Handler().postDelayed(new Runnable() {
+            // 为了减少代码使用匿名Handler创建一个延时的调用
+            public void run() {
+                SplashActivity.this.onClick();
+            }
+        }, SPLASH_DISPLAY_LENGHT);   //5秒，够用了吧
+    }
+
+    @Override
+    protected String[] getRequestUrls() {
+        return new String[0];
+    }
+
+    @OnClick(R.id.btn_start)
+    public void onClick() {
+        Intent i = new Intent(SplashActivity.this, MainActivity.class);
+        //通过Intent打开最终真正的主界面Main这个Activity
+        startActivity(i);    //启动Main界面
+        finish();    //关闭自己这个开场屏
+    }
+}
